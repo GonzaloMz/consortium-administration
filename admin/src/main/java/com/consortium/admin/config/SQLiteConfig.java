1420f588f7d4aba4c22b45159b1bc4be157e7785
Gonzalo Martinez <gonzalom312@gmail.com>
2026-03-23 16:24:07 -0300

    fix warning

--- admin/src/main/java/com/consortium/admin/config/SQLiteConfig.java
+++ admin/src/main/java/com/consortium/admin/config/SQLiteConfig.java
@@ -1,6 +1,5 @@
 package com.consortium.admin.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
